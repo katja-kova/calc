25231c9dac6752754404b7d7c0982c2043add41d
Sara-Ferrer <saraferrer@gmail.com>
2021-02-19 17:47:42 +0100

    Sara- Layout-Styles-First Operations

---
+++ action.js
@@ -0,0 +1,30 @@
+
+const buttons = document.querySelectorAll('button');
+
+const display = document.querySelector('.display');
+ 
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', calculate);
+});
+ 
+// calculate function
+function calculate(event) {
+
+  const clickedButtonValue = event.target.value;
+ 
+  if (clickedButtonValue === '=') {
+    
+    if (display.value !== '') {
+   
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === 'C') {
+    
+    display.value = '';
+  } else {
+    
+    display.value += clickedButtonValue;
+  }
+}
+
